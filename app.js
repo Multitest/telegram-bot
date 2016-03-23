cdc2cb155c78b549769c4df2a3a7cd9bcf9a24c3
artbels <artbels@gmail.com>
2016-03-23 07:12:28 +0200

    add package.json

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var TelegramBot = require('node-telegram-bot-api');
 var geocoder = require('geocoder');
 var Promise = require('es6-promise').Promise;
 
-var token = 'token';
+var token = '178205201:AAFncviR9rjSmhHhsUzCksA1tlXdsArj5O0';
 var bot = new TelegramBot(token, {
   polling: true
 });
